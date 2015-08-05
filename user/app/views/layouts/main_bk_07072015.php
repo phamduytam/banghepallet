b4724cf2fae220b665b3f4274cf9e4db2e67e896
phamduytam <pndtam@gmail.com>
2015-08-05 14:51:19 +0700

    test multi account

--- user/app/views/layouts/main_bk_07072015.php
+++ user/app/views/layouts/main_bk_07072015.php
@@ -1,4 +1,4 @@
-
+TEST
 <!DOCTYPE HTML>
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7 " lang="en"> <![endif]-->
 <!--[if IE 7]><html class="no-js lt-ie9 lt-ie8 ie7" lang="en"> <![endif]-->
